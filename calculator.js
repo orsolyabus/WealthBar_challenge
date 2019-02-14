e23f71c69df9b9c23421dfc94d22e3b57b4b23c7
Orsolya Torokne Bus <orsolyabus@gmail.com>
2019-02-14 09:54:37 -0800

    implement calculateFutureValue

--- calculator.js
+++ calculator.js
@@ -7,7 +7,7 @@
 // inflationRate
 
 // calculate
-// afterTaxDeposit // deposit - tax based on current tax rate, TFSA
+// # afterTaxDeposit // deposit - tax based on current tax rate, TFSA
 // futureValue // net deposit , years, return rate, inflation rate
 // withdrawalTax // based on future tax rate, RRSP
 // afterTaxFutureValue
@@ -20,7 +20,12 @@ function calculateAfterTaxDeposit(rate, amount){
   return amount - calculateTax(rate, amount)
 }
 
+function calculateFutureValue(rate, amount, years) {
+  return Math.round(amount*(1+rate/100)**years)
+}
+
 module.exports = {
   calculateTax,
-  calculateAfterTaxDeposit
+  calculateAfterTaxDeposit,
+  calculateFutureValue
 }
--- calculator.test.js
+++ calculator.test.js
@@ -1,18 +1,27 @@
-const { calculateTax, calculateAfterTaxDeposit } = require("./calculator")
+const { calculateTax, calculateAfterTaxDeposit, calculateFutureValue } = require("./calculator")
 {
-console.log("testing 'calculateTax'")
-const currentTaxRate = 40;
-const deposit = 1000;
-const tax = calculateTax(currentTaxRate, deposit)
-assertEqual(tax, 400)
+  console.log("testing 'calculateTax'")
+  const currentTaxRate = 40;
+  const deposit = 1000;
+  const tax = calculateTax(currentTaxRate, deposit)
+  assertEqual(tax, 400)
 }
 
 {
-console.log("testing 'calculateAfterTaxDeposit'")
-const currentTaxRate = 40;
-const deposit = 1000;
-const afterTaxDeposit = calculateAfterTaxDeposit(currentTaxRate, deposit)
-assertEqual(afterTaxDeposit, 600)
+  console.log("testing 'calculateAfterTaxDeposit'")
+  const currentTaxRate = 40;
+  const deposit = 1000;
+  const afterTaxDeposit = calculateAfterTaxDeposit(currentTaxRate, deposit)
+  assertEqual(afterTaxDeposit, 600)
+}
+
+{
+  console.log("testing 'calculateFutureValue'")
+  const currentReturnRate = 6;
+  const deposit = 1000;
+  const years = 20;
+  const futureValue = calculateFutureValue(currentReturnRate, deposit, years)
+  assertEqual(futureValue, 3207)
 }
 
 function assertEqual( check , base){
